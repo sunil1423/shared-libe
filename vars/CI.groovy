f09afe5db56d5b25a329591e4aa73b145fe51580
Sunil Kumar Sahoo <116293728+sunil1423@users.noreply.github.com>
2025-03-07 11:58:45 +0530

    Update CI.groovy

--- vars/CI.groovy
+++ vars/CI.groovy
@@ -1,1 +1,51 @@
 
+def call(Map pipeParams) {
+    pipeline {
+        agent any
+ 
+	environment {
+	    REPO_URL = "${pipeParams.scmUrl}"
+	    BRANCH_NAME = "${pipeParams.branch}"
+	    GIT_CRED = "${pipeParams.gitCredId}"
+	    APP_NAME = "${pipeParams.appName}"
+	}
+ 
+        stages {
+            stage('Initialize') {
+                steps {
+                    script {
+                        echo "Initializing Git Repository"
+                        git branch: BRANCH_NAME, credentialsId: GIT_CRED, url: REPO_URL
+                    }
+                }
+            }
+ 
+            stage('Build') {
+                steps {
+                    script {
+                        echo "Building ${APP_NAME} application of ${BRANCH_NAME} branch"
+                    }
+                }
+            }
+ 
+            stage('Test') {
+                steps {
+                    script {
+                        echo "Testing ${APP_NAME} application of ${BRANCH_NAME} branch"
+                    }
+                }
+            }
+        }
+ 
+        post {
+            success {
+                echo "CI Pipeline executed successfully for ${BRANCH_NAME} branch"
+		build job: "MultiBranch-CD-Pipeline(SL)/${BRANCH_NAME}", wait: true
+            }
+ 
+            failure {
+                echo "CI Pipeline execution failed for ${BRANCH_NAME} branch"
+            }
+        }
+    }
+}
